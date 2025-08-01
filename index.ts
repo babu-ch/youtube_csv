bc9ec7657bd44e144ad3f6456f53a5fa50f67c4b
babu-ch <mashirohira@gmail.com>
2025-08-01 15:49:53 +0900

    gunshi

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import {schema} from "./lib/config"
 import {Args, CommandContext, define} from "gunshi";
 import {main} from "./lib/main";
+import z from "zod";
 
 const args = {
   channelId: {
@@ -11,7 +12,7 @@ const args = {
     type: 'number',
     description: '1回取得ごとにsleepするms',
     default: 100,
-    parse: v => schema.shape.waitMs.parse(v)
+    parse: v => z.number().positive().parse(v)
   },
   pageToken: {
     type: 'string',
@@ -21,17 +22,20 @@ const args = {
   maxPage: {
     type: 'number',
     description: '取得するページ数',
-    default: 0
+    default: 0,
+    parse: v => z.number().min(0).parse(v)
   },
   output: {
     type: 'string',
     description: 'ファイル名を指定したい場合 ex:test.csv',
     default: 'output.csv',
+    parse: v => z.string().regex(/.+\.csv$/).parse(v)
   },
   fields: {
     type: 'string',
     description: '取得するフィールド カラム名:アクセスするフィールド をカンマ区切りで指定',
-    default: 'id:snippet.resourceId.videoId,title:snippet.title,description:snippet.description,thumbnail:snippet.thumbnails.default.url,published:snippet.publishedAt'
+    default: 'id:snippet.resourceId.videoId,title:snippet.title,description:snippet.description,thumbnail:snippet.thumbnails.default.url,published:snippet.publishedAt',
+    parse: v => z.string().regex(/^(\w+:[\w.]+,?)+$/).parse(v)
   },
   // memo 指定時channel無視するなら全部optionにした方がいいか...
   playListId: {

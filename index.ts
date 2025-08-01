6621a53ebeb6204a7908eaf8b42c49a1a25441ba
babu-ch <mashirohira@gmail.com>
2025-08-01 17:28:55 +0900

    gunshi: doc

--- index.ts
+++ index.ts
@@ -11,11 +11,11 @@ $ npm run start -- <channelId> --maxPage=1
 npm run start -- <channelId>
 
 # pageTokenを指定してページの途中から取得(append)
-node run start -- <ch> --output=test.csv --pageToken=token --maxPage=5
+node run start -- <channelId> --output=test.csv --pageToken=token --maxPage=5
 
 # 取得フィールドを変更
 # フィールドはこちらから確認 https://developers.google.com/youtube/v3/docs/playlistItems?hl=ja#resource
-node run start -- <ch> --output=test.csv --maxPage=1 --fields=id:snippet.resourceId.videoId,title:snippet.title,published:snippet.publishedAt
+node run start -- <channelId> --output=test.csv --maxPage=1 --fields=id:snippet.resourceId.videoId,title:snippet.title,published:snippet.publishedAt
 `
 
 const args = {
